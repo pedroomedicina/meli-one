95a17c2a850127e4527737afca64c45a43f49d8f
Pedro Medina Faria <pedromedina190@gmail.com>
2023-02-12 13:34:47 -0300

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div>
       <header>
         <h1>The app works!</h1>
-        <Typography variant="h1">Material UI kit works!</Typography>
+        <Typography>Material UI kit works!</Typography>
       </header>
     </div>
   );
